64f8e1b164c9b90a65d0ab0569a6ff5b9e335a46
Michael <m0n5terpanerai@gmail.com>
2021-06-14 14:21:11 +0800

    add

--- database/firebaseDB.js
+++ database/firebaseDB.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDodEYp_Ys0ko7Qw4rU4jPrzxsWkuVXnTQ",
-    authDomain: "pcmob4-3.firebaseapp.com",
-    projectId: "pcmob4-3",
-    storageBucket: "pcmob4-3.appspot.com",
-    messagingSenderId: "1022696686084",
-    appId: "1:1022696686084:web:793bdbff81ffbff8272b51"
-  };
+  apiKey: "AIzaSyDodEYp_Ys0ko7Qw4rU4jPrzxsWkuVXnTQ",
+  authDomain: "pcmob4-3.firebaseapp.com",
+  projectId: "pcmob4-3",
+  storageBucket: "pcmob4-3.appspot.com",
+  messagingSenderId: "1022696686084",
+  appId: "1:1022696686084:web:793bdbff81ffbff8272b51"
+};
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
--- screens/chat.js
+++ screens/chat.js
@@ -4,9 +4,63 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { GiftedChat, Avatar } from "react-native-gifted-chat"; 
 import firebase from "../database/firebaseDB";
 
+const db = firebase.firestore().collection("messages");
+const auth = firebase.auth();
+
 export default function Chat({ navigation }) {
   const [messages, setMessages] = useState([]);
 
+  useEffect(() => {
+
+    navigation.setOptions({
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <Avatar
+            rounded
+            source={{
+              uri: auth?.currentUser?.photoURL,
+            }}
+          />
+        </View>
+      ),
+      headerRight: () => (
+          <TouchableOpacity onPress={logout}>
+            <MaterialIcons name="logout" size={24} color="grey" />
+          </TouchableOpacity>                    
+      ),
+    });       
+
+    const unsubscribe = db
+      .orderBy("createdAt", "desc")
+      .onSnapshot((collectionSnapshot) => {    
+        const serverMessages = collectionSnapshot.docs.map((doc) => {
+          const data = doc.data();
+          console.log(data);
+          const returnData = {
+            ...doc.data(),
+            createdAt: new Date(data.createdAt.seconds * 1000),
+          };
+          return returnData;
+        });
+        setMessages(serverMessages);
+      });
+
+      return () => {
+        unsubscribe();
+      };
+
+  }, []);
+
+  function logout() {
+    auth.signOut();
+  }
+
+  function sendMessages(newMessages) {
+    console.log(newMessages);
+    const newMessage = newMessages[0];
+    db.add(newMessage);
+  }
+/*
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -42,6 +96,7 @@ export default function Chat({ navigation }) {
         // An error happened.
       });
   }
+*/ 
   return (
     <GiftedChat
       messages={messages}
